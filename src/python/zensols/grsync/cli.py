8b74014a9ef4bb1274aa542251f4999f021d97a8
Paul Landes <landes@mailc.net>
2018-09-14 12:34:07 -0500

    missing configuration file fix

--- src/python/zensols/grsync/cli.py
+++ src/python/zensols/grsync/cli.py
@@ -1,21 +1,15 @@
-import os
-from zensols.actioncli import OneConfPerActionOptionsCli
-from zensols.grsync import AppConfig
-from zensols.grsync import DistManager
+from zensols.actioncli import OneConfPerActionOptionsCliEnv
+from zensols.grsync import (
+    AppConfig,
+    DistManager,
+)
 
-CONF_ENV_VAR = 'GRSYNCRC'
 
-
-# recommended app command line
-class ConfAppCommandLine(OneConfPerActionOptionsCli):
+class ConfAppCommandLine(OneConfPerActionOptionsCliEnv):
     """Command line entry point.
 
     """
     def __init__(self):
-        if CONF_ENV_VAR in os.environ:
-            default_config_file = os.environ[CONF_ENV_VAR]
-        else:
-            default_config_file = '%s/.grsyncrc' % os.environ['HOME']
         dist_dir_op = ['-d', '--distdir', False,
                        {'dest': 'dist_dir',
                         'metavar': 'DIRECTORY',
@@ -27,7 +21,7 @@ class ConfAppCommandLine(OneConfPerActionOptionsCli):
                         {'dest': 'wheel_dependency',
                          'default': 'zensols.grsync',
                          'metavar': 'STRING',
-                         'help': 'the wheel dependency (you probably don\'t want to set this)'}]
+                         'help': 'used to create the wheel dep files'}]
         cnf = {'executors':
                [{'name': 'distribution',
                  'executor': lambda params: DistManager(**params),
@@ -42,19 +36,14 @@ class ConfAppCommandLine(OneConfPerActionOptionsCli):
                               'doc': 'build out a distribution',
                               'opts': [dist_dir_op, target_dir_op]}]}],
                'config_option': {'name': 'config',
+                                 'expect': False,
                                  'opt': ['-c', '--config', False,
                                          {'dest': 'config', 'metavar': 'FILE',
-                                          'default': default_config_file,
                                           'help': 'configuration file'}]},
                'whine': 1}
         super(ConfAppCommandLine, self).__init__(
-            cnf, pkg_dist='zensols.grsync')
-
-    def _create_config(self, config_file, default_vars):
-        defs = {}
-        defs.update(default_vars)
-        defs.update(os.environ)
-        return AppConfig(config_file=config_file, default_vars=defs)
+            cnf, config_env_name='grsyncrc', config_type=AppConfig,
+            pkg_dist='zensols.grsync')
 
 
 def main():
